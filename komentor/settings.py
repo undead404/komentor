4902b74a0df86ea358706ea18f8bd77f3d7da2b0
Vitalii Shukach <brute18@gmail.com>
2017-05-02 19:58:34 +0300

    collected static

--- komentor/settings.py
+++ komentor/settings.py
@@ -119,10 +119,12 @@ USE_L10N = True
 USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
