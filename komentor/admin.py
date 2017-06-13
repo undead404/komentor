fa853679b868c902d919e3aaf4405edbf1e8845d
undead404 <brute18@gmail.com>
2017-06-14 00:00:31 +0300

    admin panel fix

--- komentor/admin.py
+++ komentor/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Comment, Document
+from .models import Comment, Commenter, Document, Site, Vote
 
 admin.site.register(Comment)
+admin.site.register(Commenter)
 admin.site.register(Document)
+admin.site.register(Site)
+admin.site.register(Vote)
